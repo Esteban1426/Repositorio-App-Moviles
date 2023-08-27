7d3b2d2d3a192e4b78168458ba8d13ee9faac950
Esteban Bustamante <esteban.bustamanter@uniagustiniana.edu.co>
2023-08-26 19:09:43 -0500

    Metiendo Taller 2 Sobre Hilos/Threads

---
+++ TRABAJO 2/src/Clases/Actividad_2.java
@@ -0,0 +1,11 @@
+package Clases;
+
+public class Actividad_2 extends Thread
+{
+	@Override
+	public void run()
+	{	
+		System.out.println("DATOS CANCION: ");
+		System.out.println(" NOMBRE: Pompeli\n AUTOR: Dan Smith\n GENERO: Rock Alternativo\n PRODUCTORES: Dan Smith y Mark Crew\n PUBLICACION: 24 de Febrero de 2013\n DISCOGRAFICA: Virgin");
+	}
+}
---
+++ TRABAJO 2/src/Clases/Clase_Main.java
@@ -0,0 +1,24 @@
+package Clases;
+
+public class Clase_Main 
+{
+	public static void main(String[] args) 
+	{
+		Actividad_1 Musica = new Actividad_1();
+		Actividad_2 Datos_cancion = new Actividad_2();
+		
+		Musica.Cancion("Song.wav");
+		Musica.start();
+		Datos_cancion.start();
+		
+		
+		try{
+            Thread.sleep(70000);
+            Musica.AcabarCancion();
+        } 
+		catch(InterruptedException e)
+		{
+            e.printStackTrace();
+		}
+	}
+}
